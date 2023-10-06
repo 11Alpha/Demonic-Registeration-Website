bff8983f80f665d4fe19e1c26bffed42bd1ba9b4
aniket <aniketnanda11@gmail.com>
2023-10-06 16:07:08 +0530

    json error

--- app.js
+++ app.js
@@ -1,70 +1,75 @@
 require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
-const request = require("request");
 const https = require("https");
 
 const app = express();
 
 app.use(express.static(__dirname + "/public/"));
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/",function(req,res){
-    res.sendFile(__dirname+"/signup.html");
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/signup.html");
 });
 
-app.post("/",function(req,res){
-    const firstname = req.body.fname;
-    const lastname = req.body.lname;
-    const email = req.body.email;
+app.post("/", function (req, res) {
+  const firstname = req.body.fname;
+  const lastname = req.body.lname;
+  const email = req.body.email;
 
-    console.log(firstname,lastname,email);
+  console.log(firstname, lastname, email);
 
-    const data = {
-        members: [
-            {
-                email_address: email,
-                status: "subscribed",
-                merge_fields:{
-                    FNAME: firstname,
-                    LNAME: lastname,
-                }
+  // Perform data validation here
 
-            }
-        ]
-    };
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstname,
+          LNAME: lastname,
+        },
+      },
+    ],
+  };
 
-    const jsonData = JSON.stringify(data);
+  const jsonData = JSON.stringify(data);
 
-    const url = "https://us21.api.mailchimp.com/3.0/lists/e287d167f1"
+  const url = "https://us21.api.mailchimp.com/3.0/lists/e287d167f1/";
 
-    const options = {
-        method: "POST",
-        auth: "aniket:" + process.env.API_KEY
-    };
+  const options = {
+    method: "POST",
+    auth: "aniket:" + process.env.API_KEY,
+  };
 
+  const request = https.request(url, options, function (response) {
+    let responseBody = "";
 
-    const requests = https.request(url, options,  function(response){
-        if(response.statusCode==200){
-            res.sendFile(__dirname+"/success.html");
-        }
-        else{
-            res.sendFile(__dirname+"/failure.html");
-        };
-
-        response.on("data", function(data){
-            console.log(JSON.parse(data));
-        })
+    response.on("data", function (data) {
+      responseBody += data;
     });
 
-    requests.write(jsonData);
-    requests.end();
+    response.on("end", function () {
+      const parsedResponse = JSON.parse(responseBody);
+
+      if (response.statusCode === 200) {
+        res.sendFile(__dirname + "/success.html");
+      } else {
+        res.sendFile(__dirname + "/failure.html");
+        console.error("Mailchimp API Error:", parsedResponse);
+      }
+    });
+  });
+
+  request.write(jsonData);
+  request.end();
 });
 
-app.post("/failure",function(req,res){
-    res.redirect("/");
+app.post("/failure", function (req, res) {
+  res.redirect("/");
 });
 
-app.listen(process.env.PORT || 3000,function(){
-    console.log("Port started on 3000 !");
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Port started on 3000!");
 });
